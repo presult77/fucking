8eeefcc65fe4e01f5ce8c8e81142e0d07adfd604
RidF <presult77@gmail.com>
2022-02-03 01:02:29 +0700

    sshport

--- tipe2/setup.sh
+++ tipe2/setup.sh
@@ -121,7 +121,7 @@ sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
 apt -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109"/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 80"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/dropbear restart
@@ -159,8 +159,8 @@ socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
 
 [dropbear]
-accept = 4433
-connect = 127.0.0.1:109
+accept = 433
+connect = 127.0.0.1:143
 
 [dropbear]
 accept = 777
